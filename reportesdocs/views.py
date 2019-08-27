b11c57de2b31a8cc2e86cae394e562627ed693ea
edmundo <edmundo@Edmundo>
2019-08-26 22:32:17 -0400

    2: respuesta con json formato y lectura de archivo

--- reportesdocs/views.py
+++ reportesdocs/views.py
@@ -2,6 +2,23 @@ from django.http import HttpResponse
 
 from datetime import datetime
 
+# import para poder abrir un archivo
+
+from io import open
+import json
+
 def hi(request):
-    number = request.GET['numeros']
-    return HttpResponse(str(number))
+    
+    
+    # playlist = open('/home/edmundo/Documentos/proyectos/django/reportesdocs/playlist_02.03.04.m3u','r')
+    # texto = playlist.readlines()
+    
+
+    with open("/home/edmundo/Documentos/proyectos/django/reportesdocs/playlist_02.03.04.m3u", "r") as fichero:
+        texto = []
+        for linea in fichero:
+            texto.append(linea)
+
+    # playlist.close()
+    fichero.close()
+    return HttpResponse(json.dumps(texto), content_type='application/json')
